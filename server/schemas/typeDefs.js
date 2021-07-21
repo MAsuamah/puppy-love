8986c2c6a7fa95187f3254818e9cd31830594a08
liaof <f.liao163@gmail.com>
2021-07-21 19:39:41 -0400

    m

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,52 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
-const typeDefs = gql``;
-
-// export the typeDefs
-module.exports = typeDefs;
-
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    address: String
+    pets: [Pet]
+  }
+  type Pet {
+    _id: ID
+    name: String
+    breed: String
+    gender: String
+    age: Int
+    friends: [Pet]
+    images: [Image]
+    dates: [Date!]
+  }
+  type Dates {
+    _id: ID
+    daters: [Pet!]
+    day: String
+  }
+  type Image{
+    _id: ID
+    link: String
+    comments: [String]
+  }
+  type Comment{
+    _ID: id
+    userId: ID
+  }
   type Query {
     helloWorld: String
+    me: User
+    user(_id: ID!): User
+    date(_id: ID!, pet1: ID, pet2: ID): Date
+    pet(_id: ID!, name: String): Pet
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(email. String!, username: String!, password: String!, address: String, pets: [Pet]): Auth
+    addPet(name: String, breed: String, gender: String, age: Int )
   }
 `;
+module.exports = typeDefs;
+
+// one to many relationships in NoSQL: https://stackoverflow.com/questions/41243522/nosql-data-modeling-about-one-to-many-relationships
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const path = requier('path');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('.utils/auth');
