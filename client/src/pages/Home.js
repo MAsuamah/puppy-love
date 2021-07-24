39a30dc820f2f0022b42b4bff1d781f520c90d0c
Michelle <michey.asmah21@gmail.com>
2021-07-24 14:02:55 -0400

    hero tagline addition

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/styles/Home.css'
 import {BsHeartHalf} from 'react-icons/bs'
+import {FaArrowAltCircleDown} from 'react-icons/fa'
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <h1 id="hero-tagline">Find Your Next Playdate <br/>
         <BsHeartHalf />
       </h1>
+      <p id="meet-pups"><FaArrowAltCircleDown id="pup-arrow"/> Meet Some Pups</p>
     </div>
   )
 }
