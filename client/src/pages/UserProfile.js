100a0374d60b7d4a3bd16df7d5f9d14957389ebf
Michelle <michey.asmah21@gmail.com>
2021-07-24 15:47:23 -0400

    user-profile side img

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import '../assets/styles/UserProfile.css'
 
 const UserProfile = () => {
 
   return (
-   <h1>HI</h1>
+    <div className="user-profile">
+      <h1>HI</h1>
+      <h1>HI</h1>
+      <h1>HI</h1>
+    </div>
   );
 };
 
