f8646a4a743cdba03e75561eb659dcff5feabfe5
Michelle <michey.asmah21@gmail.com>
2021-07-24 16:48:44 -0400

    user profile icons

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import '../assets/styles/UserProfile.css'
+import {FaRegUserCircle} from 'react-icons/fa';
+import {FiMail} from 'react-icons/fi';
+import {MdLocationCity} from 'react-icons/md';
 
 const UserProfile = () => {
 
   return (
     <div className="user-profile">
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
+      <div id="user-info">
+        <div className="user-icons">  
+          <p><FaRegUserCircle /> Username</p>
+        </div>
+        <div className="user-icons">    
+          <p><FiMail /> Email</p>
+        </div>
+        <div className="user-icons">       
+          <p><MdLocationCity /> City</p>
+        </div>
+      </div>
     </div>
   );
 };
