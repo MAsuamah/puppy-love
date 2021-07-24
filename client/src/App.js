96f63ef82f0a8f2a8eb19638eb21b0500532970b
Michelle <michey.asmah21@gmail.com>
2021-07-23 20:37:40 -0400

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient } from '@apollo/client';
 function App() {
 return (
-    <h1>hello</h1>
+    <Router>
+        <>
+        <Navbar />
+        <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+        </>
+    </Router>
 )
 }
 export default App;
