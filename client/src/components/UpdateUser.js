7edb43faf94de989e5dd4e08740f9f46e4188d41
Michelle <michey.asmah21@gmail.com>
2021-07-24 21:15:22 -0400

    update user styling

--- client/src/components/UpdateUser.js
+++ client/src/components/UpdateUser.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
 
 function UpdateUser() {
   const [lgShow, setLgShow] = useState(false);
+
+  const updateImg = {
+    width: '700px',
+    marginBottom: '15px',
+    marginLeft: '35px'
+  }
 
   return (
     <>
@@ -19,7 +27,26 @@ function UpdateUser() {
             Update Account
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body>...</Modal.Body>
+        <Modal.Body>
+          <img style={updateImg} src={require(`../assets/images/josh-hild-tkn_izTEVGo-unsplash.jpg`).default} alt="dog owner holding their dog over there shoulder"></img>
+          <Form>
+            <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+              <Form.Control type="email" placeholder="name@example.com" />
+            </FloatingLabel>
+
+            <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
+
+            <FloatingLabel controlId="floatingCity" label="City" className="mb-3">
+              <Form.Control type="city" placeholder="City" />
+            </FloatingLabel>
+
+            <Button variant="dark" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
