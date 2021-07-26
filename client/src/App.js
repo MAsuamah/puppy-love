46e0dbeff97badde2e93c71fda55844245d753fc
Andre M <andre.ji.moseley@gmail.com>
2021-07-26 19:35:34 -0400

    first attempt

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,14 @@ import { ApolloProvider, ApolloClient } from '@apollo/client';
 function App() {
 	return (
 		<Router>
-				<>
-					<Navbar />
+
+					<NavBar />
 					<Switch>
 						<Route exact path='/' component={Home} />
 						<Route exact path='/user-profile' component={UserProfile} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
 					</Switch>
-				</>
+
 		</Router>
 	)
 }
