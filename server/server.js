a187d9159296fad3b2ac96ef1e20b7b5bbe74553
Michelle <michey.asmah21@gmail.com>
2021-07-27 22:56:15 -0400

    typo in server

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../clent/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
  }
 
 app.get('*', (req, res) => {
