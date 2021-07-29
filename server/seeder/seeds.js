fb98be356a8966e0698701f1939a30ff1a6be90c
Michelle <michey.asmah21@gmail.com>
2021-07-29 17:11:01 -0400

    seed image

--- server/seeder/seeds.js
+++ server/seeder/seeds.js
@@ -77,13 +77,34 @@ db.once('open', async () => {
       breed: 'Golden Retriever',
       gender: 'Male',
       age: 6
+    },
+    {
+      name: 'Sullivan',
+      breed: 'Bernese Mountain Dog',
+      gender: 'Male',
+      age: 3
+    },
+    {
+      name: 'Daisy',
+      breed: 'Saint Benard',
+      gender: 'Female',
+      age: 6
     }
   ]
-  
-  const seedDogs = await Dog.collection.insertMany(dogData);
-  console.log(seedDogs)
 
+  const seedDogs = await Dog.collection.insertMany(dogData);
+
+  const dogImages = [{
+    link:'https://images.unsplash.com/photo-1574273443477-87bf272e5100?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1504&q=80',
+  }]
+
+  const seedImages = await Dog.collection.insertMany(dogImages);
+  
+  
+  
   console.log(createdUsers);
+  console.log(seedDogs)
+  console.log(seedImages)
   process.exit(0);
 })
 
